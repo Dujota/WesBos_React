b78b2c70c2d5afdd2882299e139abfa3f8952fbd
Denis <ddujota@internationalsafety.com>
2018-06-03 19:24:22 -0400

    filled in react component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,13 +1,6 @@
-import React, { Component } from 'react'
-import { render  } from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import "./css/style.css";
 
-class StorePicker extends Component {
-  render() {
-    return <p>Hello !</p>
-  }
-}
-
-render ( <StorePicker />, document.querySelector('#main'));
-
-
-
+render(<StorePicker />, document.querySelector("#main"));
