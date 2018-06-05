be55d4bd55a457c01d7e06adedbd21b01027cf9e
Denis <ddujota@internationalsafety.com>
2018-06-04 22:16:54 -0400

    setting state

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
 
 export class Inventory extends Component {
   render() {
     return (
-      <div>
-        <p>Inventory</p>
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm />
       </div>
     );
   }
 }
-
 export default Inventory;
